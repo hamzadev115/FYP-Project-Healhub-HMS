1976a555443b9be338beead4116119e0f8d4febc
Ali Hamza <144887019+hamzadev115@users.noreply.github.com>
2023-09-13 13:07:25 +0500

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,65 @@
+        <!-- Footer Section -->
+        <footer class="text-lg-start text-muted footer">
+            <div class="container text-center text-md-start mt-5">
+                <div class="row mt-3">
+                    <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                        <img src="images/footer.png" class="footerlogo">
+                        <p class="details">
+                            At Healhub, we are passionate about our mission to deliver healthcare
+                            services to patients that go above and beyond anything they’ve experienced before.
+                        </p>
+                    </div>
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4 justify-content-start">
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Account
+                        </h6>
+                        <p>
+                            <a href="healhub/admin/index.php" class="text-reset">Admin</a>
+                        </p>
+                        <p>
+                            <a href="healhub/doctor/index.php" class="text-reset">Doctor</a>
+                        </p>
+                        <p>
+                            <a href="healhub/user-login.php" class="text-reset">Patient</a>
+                        </p>
+                    </div>
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">
+                            Useful links
+                        </h6>
+                        <p>
+                            <a href="about.php" class="text-reset">About</a>
+                        </p>
+                        <p>
+                            <a href="doctor.php" class="text-reset">Doctors </a>
+                        </p>
+                        <p>
+                            <a href="contact.php" class="text-reset">Contact</a>
+                        </p>
+                        <p>
+                            <a href="services.php" class="text-reset">Services</a>
+                        </p>
+                    </div>
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                        <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+                        <p class="address"><i class="fas fa-home me-3"></i>La Salle Rd, Peer Khurshid Colony
+                            Multan,Pakistan</p>
+                        <p>
+                            <i class="fas fa-envelope me-3"></i>
+                            <a href="mailto:info@smh.com">
+                                info@smh.com </a>
+                        </p>
+                        <p><i class="fas fa-phone me-3"></i> <a href="tel:+923049603842">+92 3049603842</p></a>
+                        <p><i class="fas fa-phone me-3"></i><a href="tel:+923187738842"> +92 3187738842</p></a>
+                    </div>
+                </div>
+            </div>
+            </section>
+
+            <div class="text-center p-4 fs3 Copyright">
+                © 2023 Copyright:
+                <a class="text-reset fw-bold">HEALHUB.COM</a>
+            </div>
+        </footer>
+
+        <!-- Footer Section end -->
---
+++ include/header.php
@@ -0,0 +1,69 @@
+<!-- Top Header -->
+<section class="d-flex justify-content-center justify-content-lg-between p-2 border-bottom Top-header">
+    <div class="me-5 d-lg-block t-location">
+        <i class="fa fa-location-arrow" style="margin-left: 54px;"><span>La Salle Rd, Peer Khurshid Colony
+                Multan,Pakistan</span></i>
+    </div>
+    <div class="icons">
+        <a href="" class="me-4 text-reset">
+            <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+            <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+            <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+            <i class="fab fa-linkedin"></i>
+        </a>
+    </div>
+</section>
+
+<!-- Main Header -->
+<div class="mainheader">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">
+                <img src="images/Healhub.png" alt="" srcset="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars navbar-toggler-icon"></i>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="about.php">About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="services.php">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="doctor.php">Doctors</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact Us</a>
+                    </li>
+                    <li class="nav-item appointment">
+                        <a class="nav-link" href="healhub/user-login.php" style="text-decoration:none;">Appointment</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            My Account
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="healhub/admin/index.php" target="_blank">Admin</a></li>
+                            <li><a class="dropdown-item" href="healhub/doctor/index.php" target="_blank">Doctor</a></li>
+                            <li><a class="dropdown-item" href="healhub/user-login.php" target="_blank">Patient</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
