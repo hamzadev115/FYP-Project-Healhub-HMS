eaa32630bc32f893ebf222c95c8fca8f866d2873
Ali Hamza <144887019+hamzadev115@users.noreply.github.com>
2023-09-13 13:13:28 +0500

    Add files via upload

---
+++ User Panel Assets/include/header.php
@@ -0,0 +1,121 @@
+<style>
+.navbar .navbar-header .sidebar-toggler {
+    font-size: 15px;
+    padding-right: 15px;
+    color: #fff;
+}
+
+header .navbar-header .sidebar-toggler,
+header .navbar-header .sidebar-mobile-toggler {
+    color: #fff !important;
+}
+
+.app-sidebar-fixed .navbar .navbar-header {
+    background-color: #104163 !important;
+}
+
+h2 {
+    color: #104163;
+    font-family: 'Montserrat';
+    font-size: 20px;
+    font-weight: 500;
+}
+
+.navbar-brand h2 {
+    font-size: 18px;
+}
+
+.dropdown-menu.dropdown-dark {
+    background-color: #000;
+    border: 1px solid #1A1C1E;
+}
+
+.dropdown-menu li a {
+    color: #fff !important;
+    font-family: 'Montserrat';
+}
+
+header .navbar-header .menu-toggler {
+    color: #fff !important;
+}
+</style>
+
+
+<?php error_reporting(0);?>
+<header class="navbar navbar-default navbar-static-top">
+    <!-- start: NAVBAR HEADER -->
+    <div class="navbar-header">
+        <a href="#" class="sidebar-mobile-toggler pull-left hidden-md hidden-lg" class="btn btn-navbar sidebar-toggle"
+            data-toggle-class="app-slide-off" data-toggle-target="#app" data-toggle-click-outside="#sidebar">
+            <i class="ti-align-justify"></i>
+        </a>
+        <a class="navbar-brand" href="../healhub/dashboard.php">
+            <h2
+                style="padding-top:21%; font-weight: 600; color:#fff; font-family: 'Montserrat'; text-transform: uppercase; margin-top:-12px; font-size: 25px;">
+                Healhub</h2>
+        </a>
+        <a href="#" class="sidebar-toggler pull-right visible-md visible-lg" data-toggle-class="app-sidebar-closed"
+            data-toggle-target="#app">
+            <i class="ti-align-justify"></i>
+        </a>
+        <a class="pull-right menu-toggler visible-xs-block" id="menu-toggler" data-toggle="collapse"
+            href=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <i class="ti-view-grid"></i>
+        </a>
+    </div>
+    <!-- end: NAVBAR HEADER -->
+    <!-- start: NAVBAR COLLAPSE -->
+    <div class="navbar-collapse collapse">
+        <ul class="nav navbar-right">
+            <!-- start: MESSAGES DROPDOWN -->
+            <li style="padding-top:5% ">
+                <h2>Healhub: Health & Wellness for ALL</h2>
+            </li>
+
+
+            <li class="dropdown current-user">
+                <a href class="dropdown-toggle" data-toggle="dropdown">
+                    <img src="assets/images/images.jpg"> <span class="username">
+
+
+
+                        <?php $query=mysqli_query($con,"select fullName from users where id='".$_SESSION['id']."'");
+while($row=mysqli_fetch_array($query))
+{
+	echo $row['fullName'];
+}
+									?> <i class="ti-angle-down"></i></i></span>
+                </a>
+                <ul class="dropdown-menu dropdown-dark">
+                    <li>
+                        <a href="edit-profile.php">
+                            My Profile
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="change-password.php">
+                            Change Password
+                        </a>
+                    </li>
+                    <li>
+                        <a href="logout.php">
+                            Log Out
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <!-- end: USER OPTIONS DROPDOWN -->
+        </ul>
+        <!-- start: MENU TOGGLER FOR MOBILE DEVICES -->
+        <div class="close-handle visible-xs-block menu-toggler" data-toggle="collapse" href=".navbar-collapse">
+            <div class="arrow-left"></div>
+            <div class="arrow-right"></div>
+        </div>
+        <!-- end: MENU TOGGLER FOR MOBILE DEVICES -->
+    </div>
+
+
+    <!-- end: NAVBAR COLLAPSE -->
+</header>
---
+++ User Panel Assets/include/sidebar.php
@@ -0,0 +1,95 @@
+<style>
+#sidebar {
+    background-color: #104163 !important;
+}
+
+#sidebar>div nav>ul {
+    background-color: #104163 !important;
+    font-family: 'Montserrat';
+    border: none;
+}
+
+#sidebar nav>ul>li>a .item-inner:hover {
+    background-color: #084D22 !important;
+}
+
+#sidebar nav>ul>li>a .item-inner {
+    font-size: 16px !important;
+    color: #fff !important;
+    font-weight: 500;
+}
+
+#sidebar nav>ul>li>a .item-media>i {
+    color: #fff !important;
+}
+
+.item-content:hover {
+    background-color: #084D22;
+    color: #fff;
+}
+
+#sidebar nav>ul>li>a .item-inner {
+    border: none !important;
+}
+</style>
+<div class="sidebar app-aside" id="sidebar">
+    <div class="sidebar-container perfect-scrollbar">
+
+        <nav>
+            <ul class="main-navigation-menu">
+                <li>
+                    <a href="dashboard.php">
+                        <div class="item-content">
+                            <div class="item-media">
+                                <i class="ti-home"></i>
+                            </div>
+                            <div class="item-inner">
+                                <span class="title"> Dashboard </span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="book-appointment.php">
+                        <div class="item-content">
+                            <div class="item-media">
+                                <i class="ti-pencil-alt"></i>
+                            </div>
+                            <div class="item-inner">
+                                <span class="title"> Book Appointment </span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="appointment-history.php">
+                        <div class="item-content">
+                            <div class="item-media">
+                                <i class="ti-list"></i>
+                            </div>
+                            <div class="item-inner">
+                                <span class="title"> Appointment History </span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="manage-medhistory.php">
+                        <div class="item-content">
+                            <div class="item-media">
+                                <i class="ti-list"></i>
+                            </div>
+                            <div class="item-inner">
+                                <span class="title"> Medical History </span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+
+            </ul>
+
+
+        </nav>
+    </div>
+</div>
